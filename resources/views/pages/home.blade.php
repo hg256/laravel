bc8c532ec3bb79e34ab63e444eb8024ae84931c2
himakeerthi <hg256@njit.edu>
2018-10-16 01:26:02 -0400

    Edit : Changed the header text for home and about pages.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
    <br/><br/>
-   <h1 style="font-size: 2.25rem;padding-left: 2rem;padding-top: 6rem;">Hello World! Welcome to my Keerthi's Homepage.</h1>
+   <h1 style="font-size: 2.25rem;padding-left: 2rem;padding-top: 6rem;">Hello World! Welcome to Keerthi's Homepage.</h1>
 
 @stop
