93f28a51bc2ac48caf075f3660e20dc77392ed0d
himakeerthi <hg256@njit.edu>
2018-10-16 01:12:47 -0400

    Added Home page, About page to the project. Also added, header, footer files as part of the html markup.

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,3 @@
+<div  class="font-weight-light">
+    &copy; 2017-2018 Learana, Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
+</div>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')
+   <br/><br/>
+   <h1 style="font-size: 2.25rem;padding-left: 2rem;padding-top: 6rem;">Hello World! Welcome to my Keerthi's About Page.</h1>
+
+@stop
